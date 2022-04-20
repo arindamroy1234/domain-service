83b851e819643e5cb090dce68ddfeaaffab6ce45
Sangita Roy <sangitaroy@Arindams-MacBook-Air.local>
2022-04-20 14:47:08 -0500

    domain service

---
+++ src/test/java/com/practice/kafkaintegration/domainservice/DomainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.kafkaintegration.domainservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
